537cbc4804601915f5bd02b76f4e9c2ebadfd19b
Mark Christison <mark.s.christison@gmail.com>
2025-04-07 23:00:14 -0700

    Filled out game

--- HanChou/HanChou.cpp
+++ HanChou/HanChou.cpp
@@ -8,6 +8,7 @@ int main(int argc, char **argv)
 {
     bool quit = false;
     string answer;
+    bool result = false;
 
     while(!quit)
     {
@@ -16,10 +17,45 @@ int main(int argc, char **argv)
         cin >> answer;
         if (answer == "Yes")
         {
-            cout << "Very good, would you like to be Han or Chou?" << endl;
+            result = false;
+            cout << "Very good, would you like to bet Han or Chou?" << endl;
+            do
+            {
+                cin >> answer;
+                if(answer != "Han" && answer != "Chou")
+                {
+                    cout << "I am sorry, my programmer is terrible, please be precise." << endl;
+                }
+            } while (answer != "Han"  && answer != "Chou");
+            
+            MSC_GamesLib::libChance::Die die(6);
+            int roll1 = die.fRoll();
+            int roll2 = die.fRoll();
+
+            cout << roll1 << ", " << roll2 << ": ";
+            if((roll1 + roll2) % 2 == 0)
+            {
+                cout << "Chou!" << endl;
+                if(answer == "Chou")
+                {
+                    result = true;
+                }
+            }
+            else
+            {
+                cout << "Han!" << endl;
+                if(answer == "Han")
+                {
+                    result = true;
+                }
+            }
+            string temp = result ? "win!" : "lose!";
+            cout << "You " << (result ? "win!" : "lose!");
+            cin >> answer;
         }
         else
         {
+            cout << "Farewell!" << endl;
             quit = true;
         }
     }
